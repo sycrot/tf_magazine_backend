a3d0d17b707652b6e6790d9e8543f8ddcbd05df3
Thiago Farias <thiagofarias99@hotmail.com>
2020-07-14 09:34:22 -0300

    Find page

--- src/main/java/com/example/tfmagazine/services/CategoryServices.java
+++ src/main/java/com/example/tfmagazine/services/CategoryServices.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.tfmagazine.domain.Category;
@@ -63,12 +66,12 @@ public class CategoryServices {
 		return repo.findAll();
 	}
 	
-	/*public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
-	} */
+	}
 	
 	public Category fromDTO(CategoryDTO obj) {
 		return new Category(obj.getId(), obj.getName());
