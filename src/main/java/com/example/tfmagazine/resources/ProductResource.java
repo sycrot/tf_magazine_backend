30877ea7d5712d04bded326c434bb05454c9d531
Thiago Farias <thiagofarias99@hotmail.com>
2020-07-17 09:20:56 -0300

    ProductResource, ProductDTO and URL

---
+++ src/main/java/com/example/tfmagazine/resources/ProductResource.java
@@ -0,0 +1,51 @@
+package com.example.tfmagazine.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tfmagazine.domain.Product;
+import com.example.tfmagazine.dto.ProductDTO;
+import com.example.tfmagazine.resources.utils.URL;
+import com.example.tfmagazine.services.ProductServices;
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProductResource {
+	
+	@Autowired
+	private ProductServices service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Product> find(@PathVariable Integer id) {
+		
+		Product obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	public ResponseEntity<Page<ProductDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="") String nome,
+			@RequestParam(value="categorias", defaultValue="") String categorias,
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+	
+		String nomeDecod = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Product> list = service.search(nomeDecod, ids, page, linesPerPage, orderBy, direction);
+		Page<ProductDTO> listDTO = list.map(obj -> new ProductDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
+
+}
--- src/main/java/com/example/tfmagazine/services/DBService.java
+++ src/main/java/com/example/tfmagazine/services/DBService.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.tfmagazine.domain.Category;
+import com.example.tfmagazine.domain.Product;
 import com.example.tfmagazine.repositories.CategoryRepository;
+import com.example.tfmagazine.repositories.ProductRepository;
 
 @Service
 public class DBService {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public void instantiateTestDatabase() throws ParseException {
 		
@@ -22,7 +27,30 @@ public class DBService {
 		Category cat3 = new Category(null, "Esporte e lazer");
 		Category cat4 = new Category(null, "Ferramentas");
 		
+		Product p1 = new Product(null, "Notebook", 2100.00);
+		Product p2 = new Product(null, "Impressora", 1400.00);
+		Product p3 = new Product(null, "Mouse", 40.00);
+		Product p4 = new Product(null, "Bola futebol", 120.00);
+		Product p5 = new Product(null, "Chave philips", 60.00);
+		Product p6 = new Product(null, "Bola basquete", 140.00);
+		Product p7 = new Product(null, "Kit ferraments", 400.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat3.getProducts().addAll(Arrays.asList(p4, p6));
+		cat4.getProducts().addAll(Arrays.asList(p5, p7));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p4.getCategories().addAll(Arrays.asList(cat3));
+		p5.getCategories().addAll(Arrays.asList(cat4));
+		p6.getCategories().addAll(Arrays.asList(cat3));
+		p7.getCategories().addAll(Arrays.asList(cat4));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7));
+		
 		
 	}
 
