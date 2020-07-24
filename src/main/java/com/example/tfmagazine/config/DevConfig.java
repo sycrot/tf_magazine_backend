c9d3a17013dbdb7e56bd548429cbdd798a752cff
Thiago Farias <thiagofarias99@hotmail.com>
2020-07-24 09:45:27 -0300

    DevConfig, SecurityConfig, SmtpEmailService

--- src/main/java/com/example/tfmagazine/config/DevConfig.java
+++ src/main/java/com/example/tfmagazine/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.example.tfmagazine.services.DBService;
+import com.example.tfmagazine.services.EmailService;
+import com.example.tfmagazine.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -26,6 +28,12 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		
 		return true;
+		
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 		
 	}
 
--- src/main/java/com/example/tfmagazine/config/TestConfig.java
+++ src/main/java/com/example/tfmagazine/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.example.tfmagazine.services.DBService;
+import com.example.tfmagazine.services.EmailService;
+import com.example.tfmagazine.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -23,6 +25,11 @@ public class TestConfig {
 		
 		return true;
 		
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 
 }
